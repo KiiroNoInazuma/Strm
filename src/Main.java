00886d73b47b423b7bef581fd53cf79e3b25463e
John Rey <52321315+KiiroNoInazuma@users.noreply.github.com>
2023-07-28 20:18:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,19 @@ public class Main {
                                       Comparator<? super T> order,
                                       BiConsumer<? super T, ? super T> minMaxConsumer) {
         List<T> list = stream.collect(Collectors.toList());
-        T min = list.stream().min(order).orElse(null);
+        
+        /*T min = list.stream().min(order).orElse(null);
         T max = list.stream().max(order).orElse(null);
-        minMaxConsumer.accept(min, max);
+        minMaxConsumer.accept(min, max);*/
+
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            T min = list.stream().min(order).orElseThrow();
+            T max = list.stream().max(order).orElseThrow();
+            minMaxConsumer.accept(min, max);
+        }
+        
         System.out.println("Все значения списка: " +
                 list.stream()
                         .map(Object::toString)
